d8626023fe521a24aea918a10f9ce69c0dac065c
ndambuki8 <ndambukikevo8@gmail.com>
2023-06-04 13:34:46 +0300

    Implemented DB Model, SUperuser - Admin and function to display data in DB

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+def book_by_id(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    return HttpResponse(f"Book: {book.title}, published on {book.pub_date}")
